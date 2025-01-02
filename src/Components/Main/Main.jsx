957fa05f095ea2e182aa99a5f438dc136a11a42d
Iganza-roy <iganzaroy55@gmail.com>
2025-01-02 19:03:15 +0300

    feat: implement Main component with greeting and card suggestions

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,5 +1,5 @@
 import './main.css';
-import assets from '../../assets';
+import { assets } from '../../assets/assets';
 
 const Main = () => {
   return (
@@ -7,6 +7,35 @@ const Main = () => {
       <div className='nav'>
         <p>Gemini</p>
         <img src={assets.user_icon} alt='user' />
+      </div>
+      <div className='main-container'>
+        <div className='greet'>
+          <p>
+            <span>Hello, Roy</span>
+          </p>
+          <p>How can I help you today?</p>
+        </div>
+        <div className='cards'>
+          <div className='card'>
+            <p>Suggest beautiful places to see on an upcoming road trip</p>
+            <img src={assets.compass_icon} alt='compass' />
+          </div>
+          <div className='card'>
+            <p>Briefly summarize content...</p>
+            <img src={assets.bulb_icon} alt='bulb' />
+          </div>
+          <div className='card'>
+            <p>Brainstorm team bonding session...</p>
+            <img src={assets.message_icon} alt='message' />
+          </div>
+          <div className='card'>
+            <p>Improve the readability of the following code</p>
+            <img src={assets.code_icon} alt='code' />
+          </div>
+        </div>
+        <div className='main-bottom'>
+          <div className='search-box'></div>
+        </div>
       </div>
     </div>
   );
