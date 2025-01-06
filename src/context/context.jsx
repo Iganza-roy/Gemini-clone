544d49b20800525b801629128fd8c787bb455fa0
Iganza-roy <iganzaroy55@gmail.com>
2025-01-06 16:45:50 +0300

    feat: enhance result formatting in context provider and update styling for result display

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -11,13 +11,25 @@ const ContextProvider = (props) => {
   const [loading, setLoading] = useState(false);
   const [resultData, setResultData] = useState('');
 
+  const delayParam = (index, next) => {};
+
   const onSent = async (prompt) => {
     setResultData('');
     setLoading(true);
     setShowResult(true);
     setrecentPrompt(input);
-    const resonse = await run(input);
-    setResultData(resonse);
+    const response = await run(input);
+    let responseArray = response.split('**');
+    let newResponse;
+    for (let i = 0; i < responseArray.length; i++) {
+      if (i === 0 || i % 2 !== 1) {
+        newResponse += responseArray[i];
+      } else {
+        newResponse += '<b>' + responseArray[i] + '</b>';
+      }
+    }
+    let newResponse2 = newResponse.split('*').join('</br>');
+    setResultData(newResponse2);
     setLoading(false);
     setInput('');
   };
