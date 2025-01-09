5300b4be5e39d160edfdb69d877fb65b3fa215c2
Iganza-roy <iganzaroy55@gmail.com>
2025-01-09 13:28:16 +0300

    feat: add Enter key functionality to send input in Main component

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -14,6 +14,12 @@ const Main = () => {
     input,
   } = useContext(Context);
 
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter' && input) {
+      onSent();
+    }
+  };
+
   return (
     <div className='main'>
       <div className='nav'>
@@ -76,6 +82,7 @@ const Main = () => {
               value={input}
               type='text'
               placeholder='Enter prompt here'
+              onKeyDown={handleKeyDown}
             />
             <div>
               <img src={assets.gallery_icon} alt='gallery' />
